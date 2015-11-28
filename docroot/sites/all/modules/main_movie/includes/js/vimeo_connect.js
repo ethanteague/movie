efa349e87dff9b52cf8fb134752e6715bb21a954
Ethan Teague <ethan.teague@civicactions.com>
2015-11-27 19:49:10 -0500

    Staying current.

---
+++ docroot/sites/all/modules/main_movie/includes/js/vimeo_connect.js
@@ -0,0 +1,10 @@
+// client ID
+// 747077747421-jso3lp2pe8vinn6k12c1p6jvhr3tehtl.apps.googleusercontent.com
+// client secret
+// POXkVs2KwYD1r-J5A2cNabhv
+
+var uploader = new MediaUploader({
+  file: content,
+  token: '747077747421-jso3lp2pe8vinn6k12c1p6jvhr3tehtl.apps.googleusercontent.com',
+});
+uploader.upload();
